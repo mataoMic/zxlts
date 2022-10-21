7ba1e2bd0facc3afd2ad8641fddabd414c42e7f8
Mt_Mic <363262184@qq.com>
2022-10-21 09:57:05 +0800

    u

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { Button } from '@arco-design/mobile-react';
-
+import Arco from '@arco-design/mobile-react';
+import '@arco-design/mobile-react/esm/style';
 export default function ButtonDemo() {
     return (
         <>
         <Button needActive>
             Primary
-        </Button>
-
-        <Button disabled style={{ marginTop: 20 }}>
-            Disabled
         </Button>
         </>
     );
